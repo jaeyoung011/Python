36afaaf96de5e62dabe7a60eea0b21d65cb4ac17
Jaeyoung Park <52947910+jaeyoung011@users.noreply.github.com>
2022-05-18 22:41:06 +0900

    Add files via upload

---
+++ programmers/Level_1/부족한금액계산하기.py
@@ -0,0 +1,16 @@
+
+
+def solution(price, money, count):
+
+    sum = 0
+
+    for i in range(1, count+1):
+        sum += price*i
+    if money > sum:
+        return 0
+    else:
+        return sum - money
+
+
+
+print(solution(3, 20, 4))
