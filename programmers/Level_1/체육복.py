5e2a659e43e158ce068f67996c5485649e905eab
Jaeyoung Park <52947910+jaeyoung011@users.noreply.github.com>
2022-05-29 19:54:27 +0900

    Update 체육복.py

--- programmers/Level_1/체육복.py
+++ programmers/Level_1/체육복.py
@@ -1,5 +1,5 @@
 
-
+# 나의 풀이( 점수75)... 이해가 안가서 다른 사람 풀이 밑에 참조
 def solution(n, lost, reserve):
 
     start = n - len(lost)
@@ -22,6 +22,9 @@ print(solution(5, [2,4], [3]))
 print(solution(3, [3], [1]))
 
 # 다른 사람 풀이
+# 여별 체육복을 가지고 있는 사람도 도난을 당할수있다..! 이거를 까먹지 말았어야한다...
+# https://rain-bow.tistory.com/30 (참조)
+# 테스트 케이스 에서 계속 실패를 해서 설명을 듣고 문제를 정확하게 파악하는것이 중요하다는걸 알았다..
 
 def solution(n, lost, reserve):
     set_reserve = set(reserve) - set(lost)
